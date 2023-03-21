26e41849ededefa395e94e606569632fd8a291c7
ameen-ansari <ameen55668@gmail.com>
2023-03-21 18:28:27 +0500

    git push

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -6,16 +6,28 @@ import "slick-carousel/slick/slick-theme.css";
 import img1 from '../../Images/Rectangle 4.svg'
 import img2 from '../../Images/Rectangle 5.svg'
 import img3 from '../../Images/Rectangle 6.svg'
+import img from '../../Images/Vector (5).svg'
 
 
 export default class SimpleSlider extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
   render() {
     const settings = {
       dots: false,
       infinite: true,
       speed: 1000,
       slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToScroll: 1,
       arrows:false,
       responsive: [
         {
@@ -49,13 +61,17 @@ export default class SimpleSlider extends Component {
 
     return (
       <div className={style.parent}>
-        <Slider {...settings}>
+        <Slider ref={(c) => (this.slider = c)} {...settings}>
           {arr.map((img, i) => {
             return (
                   <img key={i} className={style.img} alt="img" src={img} />
             );
           })}
         </Slider>
+        <div className={style.arrows}>
+          <img onClick={this.previous} src={img} alt="" />
+          <img onClick={this.next} src={img} alt="" />
+        </div>
       </div>
     );
   }
